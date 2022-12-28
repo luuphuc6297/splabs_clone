81ca3f8894c3dcff4e4d8c7f40fb5650cb903da6
luuphuc6297 <luuphuc6297@gmail.com>
2022-12-28 13:26:46 +0700

    update style

--- src/assets/background/index.js
+++ src/assets/background/index.js
@@ -3,3 +3,4 @@ export const secondBackground = '/images/background/second-background-min.png';
 export const partnerBackground = '/images/background/partner_background.png';
 export const visionBackground = '/images/background/vision-background.png';
 export const processBackground = '/images/background/process-background.png';
+export const influencerProcess = '/images/background/process-banner.png';
--- src/pages/Process/InfluencerProcess/index.js
+++ src/pages/Process/InfluencerProcess/index.js
@@ -3,14 +3,24 @@ import {
     SectionTitle,
     InfluencerProcessCard,
 } from 'components';
-import { Divider, Box, Grid } from '@mui/material';
+import { Divider, Box, Grid, styled } from '@mui/material';
 import influencer from './influencer.json';
+import { influencerProcess } from 'assets';
+import { useTheme } from '@mui/material/styles';
+const StyledProcessBanner = styled('img')(({ theme }) => ({
+    width: '100%',
+    height: '100%',
+    objectFit: 'contain',
+}));
 export const InfluencerProcess = () => {
+    const theme = useTheme();
+
     return (
         <SectionWrapper id="influencer_process" sx={{ marginBottom: 21 }}>
             <SectionTitle sx={{ textAlign: 'center' }}>
                 INFLUENCER PROCESS
             </SectionTitle>
+
             <Divider
                 sx={{
                     width: '5%',
@@ -18,6 +28,29 @@ export const InfluencerProcess = () => {
                     margin: '8px auto 56px auto',
                 }}
             />
+            <Box
+                sx={{
+                    width: 580,
+                    height: 600,
+                    marginBottom: 6,
+                    margin: '0 auto 56px auto',
+                    textAlign: 'center',
+                    [theme.breakpoints.down('md')]: {
+                        width: 520,
+                        height: 560,
+                    },
+                    [theme.breakpoints.down('sm')]: {
+                        width: 480,
+                        height: 520,
+                    },
+                    [theme.breakpoints.down('480')]: {
+                        width: 380,
+                        height: 420,
+                    },
+                }}
+            >
+                <StyledProcessBanner src={influencerProcess} loading="lazy" />
+            </Box>
             <Box>
                 <Grid container columnSpacing={2} rowSpacing={2}>
                     {influencer.payload.map((item) => (
