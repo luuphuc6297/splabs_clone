53131dd9d25c2e5eef9dfa22369534aa27a3a3d7
Tran Anh <anh.tran@blueberrylabs.com>
2022-12-28 07:22:12 +0700

    ui the team

--- src/pages/Vision/TheFullyTeam/TheOurTeam/index.js
+++ src/pages/Vision/TheFullyTeam/TheOurTeam/index.js
@@ -1,8 +1,11 @@
-import { Box, Grid } from '@mui/material';
+import { Box, Button, Grid } from '@mui/material';
 import { SectionTitle, SectionWrapper, TheOurTeamCard } from 'components';
+import { useState } from 'react';
 import ourTeam from './ourTeam.json';
 
 export const TheOurTeam = () => {
+    const [isShowMore, setIsShowMore] = useState(true);
+
     return (
         <SectionWrapper id="the_our_team" sx={{ marginBottom: 21 }}>
             <SectionTitle
@@ -17,17 +20,37 @@ export const TheOurTeam = () => {
             </SectionTitle>
 
             <Grid container columnSpacing={6} rowSpacing={4}>
-                {ourTeam.payload.map((item) => (
-                    <Grid item xs={12} sm={6} md={4} key={item.id}>
-                        <TheOurTeamCard
-                            id={item.id}
-                            name={item.name}
-                            position={item.position}
-                            descriptions={item.descriptions}
-                        />
-                    </Grid>
-                ))}
+                {ourTeam.payload
+                    .slice(0, isShowMore ? 6 : undefined)
+                    .map((item) => (
+                        <Grid item xs={12} sm={6} md={4} key={item.id}>
+                            <TheOurTeamCard
+                                id={item.id}
+                                name={item.name}
+                                position={item.position}
+                                descriptions={item.descriptions}
+                            />
+                        </Grid>
+                    ))}
             </Grid>
+            <Box sx={{ display: 'flex', justifyContent: 'center', mt: 6 }}>
+                <Button
+                    onClick={() => setIsShowMore((preState) => !preState)}
+                    sx={{
+                        color: '#333333',
+                        background: '#ffffff',
+                        height: 48,
+                        width: 130,
+                        padding: '15px',
+                        borderRadius: 30,
+                        '&:hover': {
+                            backgroundColor: '#ffffff',
+                        },
+                    }}
+                >
+                    Load more
+                </Button>
+            </Box>
         </SectionWrapper>
     );
 };
